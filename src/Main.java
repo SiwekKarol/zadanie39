79b845a36fa2f531af08c4e78d0dc2a490b34ce0
Karol Siwek <siwek.karol1992@gmail.com>
2018-10-12 16:42:38 +0200

    zadanie

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Cześć, w załączniku przesyłam rozwiązane zadanie 10.2/39. Pozdrawiam Karol");
+    }
+}
